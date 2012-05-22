df9001f68df3365c28bcade49fb82aeead173d06
Oyvind Nordsveen <oyvind.nordsveen@gmail.com>
2012-05-22 15:38:16 +0200

    Added support for JDBC monitoring

--- src/main/java/com/visma/autosysmonitor/da/MonitorFactory.java
+++ src/main/java/com/visma/autosysmonitor/da/MonitorFactory.java
@@ -2,12 +2,15 @@ package com.visma.autosysmonitor.da;
 
 import com.visma.autosysmonitor.domain.HeaderMonitor;
 import com.visma.autosysmonitor.domain.HttpGetMonitor;
+import com.visma.autosysmonitor.domain.JdbcMonitor;
 import com.visma.autosysmonitor.domain.Monitor;
 
 public class MonitorFactory {
 
 	private static final String HTTPGET = "HTTP-GET";
 	private static final String HEADER = "HEADER";
+	private static final String JDBC = "JDBC";
+	
 	
 	public static Monitor createMonitor(String line) {
 		String[] elem = line.split(";");
@@ -19,6 +22,9 @@ public class MonitorFactory {
 		}
 		if (elem[0].equalsIgnoreCase(HEADER)){
 			return  new HeaderMonitor(elem[1], elem[2], Integer.parseInt(elem[3]));
+		}
+		if (elem[0].equalsIgnoreCase(JDBC)){
+			return new JdbcMonitor(elem[1], elem[2], Integer.parseInt(elem[3]));
 		}
 		return null;
 	}
---
+++ src/main/java/com/visma/autosysmonitor/domain/JdbcMonitor.java
@@ -0,0 +1,185 @@
+package com.visma.autosysmonitor.domain;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JdbcMonitor implements Monitor {
+	private String name;
+	private String url;
+	private boolean alive;
+	private int ping;
+	private int timeout;
+
+	public JdbcMonitor(String name, String url, int timeout) {
+		this.name = name;
+		this.url = url;
+		this.timeout = timeout;
+		this.alive = false;
+		this.ping = 0;
+	}
+
+	public JdbcMonitor() {
+		this.name = "";
+		this.url = "";
+		this.timeout = 0;
+		this.alive = false;
+		this.ping = 0;
+	}
+
+	public JdbcMonitor(MonitorDTO to) {
+		this.name = to.getName();
+		this.url = to.getUrl();
+		this.timeout = to.getTimeout();
+		this.alive = to.isAlive();
+		this.ping = to.getPing();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.visma.autosysmonitor.domain.Monitor#toSystemInfoDTO()
+	 */
+	@Override
+	public MonitorDTO toSystemInfoDTO() {
+		MonitorDTO sys = new MonitorDTO(this);
+		return sys;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.visma.autosysmonitor.domain.Monitor#update()
+	 */
+	@Override
+	public void update() {
+		try {
+			Class.forName("oracle.jdbc.OracleDriver");
+			Connection conn = DriverManager.getConnection(this.url);
+			Statement stmt = conn.createStatement();
+			try {
+				ResultSet rset = stmt.executeQuery("select 1 from DUAL");
+				alive= true;
+				try {
+				} finally {
+					try {
+						rset.close();
+					} catch (Exception ignore) {
+					}
+				}
+
+			} finally {
+				stmt.close();
+			}
+		} catch (ClassNotFoundException e) {
+			alive= false;
+			e.printStackTrace();
+		} catch (SQLException e) {
+			alive= false;
+			e.printStackTrace();
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.visma.autosysmonitor.domain.Monitor#getName()
+	 */
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.visma.autosysmonitor.domain.Monitor#setName(java.lang.String)
+	 */
+	@Override
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.visma.autosysmonitor.domain.Monitor#getUrl()
+	 */
+	@Override
+	public String getUrl() {
+		return url;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.visma.autosysmonitor.domain.Monitor#setUrl(java.lang.String)
+	 */
+	@Override
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.visma.autosysmonitor.domain.Monitor#isAlive()
+	 */
+	@Override
+	public boolean isAlive() {
+		return alive;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.visma.autosysmonitor.domain.Monitor#setAlive(boolean)
+	 */
+	@Override
+	public void setAlive(boolean alive) {
+		this.alive = alive;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.visma.autosysmonitor.domain.Monitor#getPing()
+	 */
+	@Override
+	public int getPing() {
+		return ping;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.visma.autosysmonitor.domain.Monitor#setPing(int)
+	 */
+	@Override
+	public void setPing(int ping) {
+		this.ping = ping;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.visma.autosysmonitor.domain.Monitor#getTimeout()
+	 */
+	@Override
+	public int getTimeout() {
+		return timeout;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.visma.autosysmonitor.domain.Monitor#setTimeout(int)
+	 */
+	@Override
+	public void setTimeout(int timeout) {
+		this.timeout = timeout;
+	}
+
+}
